067d8e9d909e6caceb6816a4623a5ed4d0796de7
Joe Bernardi <joebernardi@hotmail.com>
2017-02-03 19:50:15 -0600

    add routes and user#index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, :only => [:new, :index]
+  resource :session, :only => [:new, :create, :destroy]
+  get 'login' => "sessions#new"
+  delete 'logout' => "sessions#destroy"
+
+  root :to => "users#index"
+
 end
--- db/migrate/20170203220938_create_users.rb
+++ db/migrate/20170203220938_create_users.rb
@@ -2,11 +2,15 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
 
-    	t.string :email
-    	t.string :password_digest
+    	t.string :email, :null => false
+    	t.string :password_digest, :null => false
 
 
       t.timestamps
     end
+
+    add_index :users, :email, :unique => true
+    add_index :users, :password_digest
+
   end
 end
